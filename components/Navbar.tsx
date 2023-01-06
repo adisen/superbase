1d79c7fecb3e4d7b2ff7ae60f955607b74fa1db0
Israel <israeleadetunji@gmail.com>
2023-01-06 13:11:25 +0100

    add tailwind configuration and implemented Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Roboto } from "@next/font/google";
+import Link from "next/link";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["700"] });
+
+const Navbar = () => {
+  return (
+    <header className="bg-black text-white py-[20px] px-[109px] flex justify-between">
+      <h1 className={`${roboto.className} text-xl uppercase`}>Superare</h1>
+      <div>
+        <ul className="flex justify-between text-base gap-10">
+          <li>
+            <Link href="/roadmap">Roadmap</Link>
+          </li>
+          <li>
+            <Link href="/team">Team</Link>
+          </li>
+          <li>
+            <Link href="/team">About</Link>
+          </li>
+          <li>
+            <Link href="/wallet">Wallet</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
