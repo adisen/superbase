0d4c5f7861da1d4653e33005373aadc394f0db57
Israel <israeleadetunji@gmail.com>
2023-01-12 22:09:37 +0100

    add partners section

---
+++ components/Partners.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react";
+import { Roboto } from "@next/font/google";
+import Image from "next/image";
+import webflow from "../assets/images/webflow.png";
+import relume from "../assets/images/Black.png";
+
+const roboto = Roboto({ weight: ["700"], subsets: ["latin"] });
+
+const Partners: FunctionComponent = () => {
+  return (
+    <div className="text-center py-20 bg-black text-white">
+      <h5 className={`${roboto.className} text-lg`}>Partners</h5>
+      <div className="flex gap-16 pt-12">
+        <Image alt="webflow logo" src={webflow} />
+        <Image alt="relume logo" src={relume} />
+        <Image alt="webflow logo" src={webflow} />
+        <Image alt="relume logo" src={relume} />
+        <Image alt="webflow logo" src={webflow} />
+        <Image alt="relume logo" src={relume} />
+        <Image alt="webflow logo" src={webflow} />
+        <Image alt="relume logo" src={relume} />
+        <Image alt="webflow logo" src={webflow} />
+        <Image alt="relume logo" src={relume} />
+        <Image alt="webflow logo" src={webflow} />
+        <Image alt="relume logo" src={relume} />
+      </div>
+    </div>
+  );
+};
+
+export default Partners;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Partners from "../components/Partners";
 
 export default function Home() {
   return (
@@ -15,9 +16,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="bg-black text-white">
         <Navbar />
         <Hero />
+        <Partners />
       </main>
     </>
   );
